7a518b09595085aefdf0e295440a4696258b47af
roynatech2544 <whiteshell2544@naver.com>
2023-09-11 18:46:00 +0900

    dolby-vendor: Add framework compability matrix

--- BoardConfigDolby.mk
+++ BoardConfigDolby.mk
@@ -3,3 +3,5 @@ DOLBY_PATH := vendor/motorola/dolby
 TARGET_VENDOR_PROP += $(DOLBY_PATH)/vendor.prop
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/dolby_framework_matrix.xml
